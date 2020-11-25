cb121bbbe155cf1290516bc206adfb0def9e0d21
Simone <simone.sichili@gmail.com>
2020-11-25 16:25:04 +0100

    Dropdown menu to fixed. No bonus.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
     // START JS
 
+    $("#nav-right > ul.list-inline-r > li").mouseenter(
+        function () {
+            $(this).find(".dropdown").addClass("active");
+        }
+    );
+
+    $("#nav-right .dropdown").mouseleave(
+        function () {
+            $(this).removeClass("active");
+        }
+    );
+
+    $(document).click(
+        function () { 
+            $("#nav-right .dropdown").removeClass("active");
+        }
+    );
 
     // END JS
 });
